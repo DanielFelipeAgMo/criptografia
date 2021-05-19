9997f4588ea056decc9e04e11cec76b5f0d21dca
Daniel Felipe A.M <danielagudelo87@gmail.com>
2021-05-19 14:07:50 -0500

    creacion del repo

---
+++ cifrado.py
@@ -0,0 +1,42 @@
+import string
+# El limite de desplazamiento es 11 sin embargo para textos largos aplica hasta 6
+desplazamiento =5
+
+def codifica (texto):
+    cifrado = ""
+    if texto == texto.upper():
+        lista = list(string.ascii_uppercase)
+    else:
+        lista = list(string.ascii_lowercase)
+
+    for car in texto:
+        if car in lista:
+            cifrado += lista[(lista.index(car)+ desplazamiento%(len(lista)))]
+        else:
+            cifrado += car
+
+    print(cifrado)
+    return cifrado
+
+def decodifica (texto):
+    decifrado = ""
+    if texto == texto.upper():
+        lista = list(string.ascii_uppercase)
+    else:
+        lista = list(string.ascii_lowercase)
+
+    for car in texto:
+        if car in lista:
+            decifrado += lista[(lista.index(car) - desplazamiento%(len(lista)))]
+        else:
+            decifrado += car
+
+    print(decifrado)
+    return decifrado
+
+if __name__ == "__main__":
+    cifrado = codifica("CUIDADO")
+    decifrado = decodifica(cifrado)
+
+
+   #
